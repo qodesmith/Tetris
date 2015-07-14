d6f86fee7c2cd07a8f59334c7e3be1dff37f64b5
Aaron Cordova <theqodesmith@gmail.com>
2015-07-14 08:13:24 -0400

    Fully functioning game.

---
+++ requires.js
@@ -0,0 +1,13 @@
+// Requires libraries.
+_          = require('underscore');
+Backbone   = require('backbone');
+Handlebars = require('handlebars');
+$          = require('jquery');
+
+// Local files to concact.
+appFile = require('./public/js/app.js');
+mainViewFile = require('./public/js/views/mainView.js');
+startViewFile = require('./public/js/views/startView.js');
+pcs = require('./public/js/pieces.js');
+cntrls = require('./public/js/controls.js');
+information = require ('./public/js/information.js');
